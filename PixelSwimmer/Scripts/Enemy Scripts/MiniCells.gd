7c17aea10e60764dcba5f9e194def92ac3c2aeed
DKRapArtist <167987183+DKRapArtist@users.noreply.github.com>
2025-11-20 16:56:29 +0200

    Bacteria Minion Update (Cant Shoot)

--- PixelSwimmer/Scripts/Enemy Scripts/MiniCells.gd
+++
@@ -1,8 +0,0 @@
-class_name MiniCells
-extends Enemy
-
-func die(source: Node) -> void:
-
-	#emit signal, points, deathsound and remove enemy
-	enemy_killed.emit(points, death_sound, source)
-	queue_free()
--- PixelSwimmer/Scripts/Laser Scripts/EnemyLaser.gd
+++
@@ -1,9 +0,0 @@
-class_name EnemyLaser
-extends Laser
-
-func _on_body_entered(body: Node2D) -> void:
-	if body is Player:
-		body.take_damage(damage) #damages player
-
-		#delete player
-		queue_free()
