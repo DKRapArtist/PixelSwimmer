89154b330ba7db87b00a3ae1ce286ceced4294e9
DKRapArtist <167987183+DKRapArtist@users.noreply.github.com>
2025-11-21 13:33:30 +0200

    Damage Text UPDATE

--- PixelSwimmer/PixelSwimmer/Scripts/Buffs Scripts/DamageBuff.gd
+++ PixelSwimmer/PixelSwimmer/Scripts/Buffs Scripts/DamageBuff.gd
@@ -2,11 +2,16 @@ class_name DamageBuff
 extends Buffs
 
 @export var duration = 20
+@export var damage_text_scene: PackedScene  # DamageX2Text.tscn
 
-func _on_body_entered(body):
-	if body is Player:
-		
+func _on_body_entered(body: Node) -> void:
+	if body.is_in_group("player"):
 		body.apply_damage_buff(20.0)
-		
-		picked_up.emit(buff_sound)
+		show_damage_x2_text(body)
 		queue_free()
+
+func show_damage_x2_text(player: Node) -> void:
+	var popup = damage_text_scene.instantiate()
+	player.get_node("DamageX2Anchor").add_child(popup)
+	popup.position = Vector2.ZERO  # centered on anchor
+	popup.show_text()
